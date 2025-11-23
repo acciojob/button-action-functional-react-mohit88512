d62c021c1655e2aa28109e3a71966603a0181fb9
Mohit <s.mohit3547@gmail.com>
2025-11-23 12:39:04 +0530

    yooo

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,19 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [text,setext] = useState()
+
+
+  function action(){
+    let value = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
+    setext(value)
+  }
+
   return (
     <div id="main">
       // Do not alter the main div
+      <button id="click" onClick={action}>click</button>
+      <p id="para">{text}</p>
     </div>
   );
 }
