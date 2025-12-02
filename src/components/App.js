4defe74ac194a6171d9082f93eafb95f062348be
Mohit <s.mohit3547@gmail.com>
2025-12-02 19:43:24 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [text,setext] = useState()
+  let [text,setext] = useState("false")
 
 
   function action(){
-    let value = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."
-    setext(value)
+    setext("true")
   }
 
   return (
     <div id="main">
       <button id="click" onClick={action}>click</button>
-      <p id="para">{text}</p>
+      {{text} && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
     </div>
   );
 }
