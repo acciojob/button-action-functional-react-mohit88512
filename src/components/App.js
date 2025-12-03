10a12f6b3cabdd756b12207839ffbbc7e9caba45
Mohit <s.mohit3547@gmail.com>
2025-12-03 12:06:52 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [text,setext] = useState("false")
-
-
-  function action(){
-    setext("true")
-  }
+  let [text,settext] = useState(false)
 
   return (
     <div id="main">
-      <button id="click" onClick={action}>click</button>
-      {{text} && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
+      <button id="click" onClick={()=>{settext(true)}}>click</button>
+      {text && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
     </div>
   );
 }
